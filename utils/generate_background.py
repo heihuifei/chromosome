a7ed776fbac82320def7c78894cb9413e85bef49
heihuifei <1730862627@qq.com>
2022-01-16 11:12:36 +0000

    feat: add function fillImageNonChromos and rotateImages for background Image's augmentation

--- utils/generate_background.py
+++ utils/generate_background.py
@@ -3,24 +3,30 @@ Author: hxp
 Introduction: 对染色体原图进行规则切割，并采用传统边缘检测方法提取不同类别的单条染色体生成数据集
 Time: 2021.11.13
 '''
-import queue
-import os
 import math
 import uuid
 import json
+import io
+import os
+import base64
 import os.path as osp
 
 import cv2
 import numpy as np
 import PIL.Image
 import PIL.ImageDraw
-from numpy.random.mtrand import choice
+from numpy.lib.ufunclike import fix
+from numpy.random.mtrand import choice, random, sample
 import pandas as pd
+import random as rd
+
+from scipy.fft import dst
 
 import image_tool as imgTool
 import generate_segmentation_dataset as gsd
 
 label_name_to_value = {"_background_": 0, "chromos": 1}
+handPadding = [70, 35]
 
 
 # function: 将labelme中的shapes字段坐标点数组转为mask(源于labelme源码)
@@ -106,13 +112,40 @@ def lblsave(filename, lbl):
         raise ValueError("[%s] Cannot save the pixel-wise class label as PNG. "
                          "Please consider using the .npy format." % filename)
 
+# function: 用于将imageData转为image
+# params: 图像的imageData数据
+# return: img_pil
+def img_data_to_pil(img_data):
+    f = io.BytesIO()
+    f.write(img_data)
+    img_pil = PIL.Image.open(f)
+    return img_pil
+
+
+# function: 将imageData的数据转为array格式的image
+# params: 图像的b64编码并被解码后的数据
+# return: image
+def img_data_to_arr(img_data):
+    img_pil = img_data_to_pil(img_data)
+    img_arr = np.array(img_pil)
+    return img_arr
+
+# function: 将imageData的b64编码数据转为array格式的image
+# params: 图像的b64编码数据
+# return: image
+def img_b64_to_arr(img_b64):
+    img_data = base64.b64decode(img_b64)
+    img_arr = img_data_to_arr(img_data)
+    return img_arr
+
 
 # function: 根据图像像素值分布，按概率分布随机生成cnt个(r,g,b)值
 # params: 图像，生成(r,g,b)个数
 # return: [[r1,g1,b1] [r2,g2,b2]...] (type=array)
 def generateRandRgbs(image, cnt):
     # 将image数组变型为的rgb像素值数组
-    imageRgb = image.reshape(image.shape[0] * image.shape[1], 3)
+    cpImage = image[handPadding[0]:(image.shape[0]-handPadding[0]), handPadding[1]:(image.shape[1]-handPadding[1]), :]
+    imageRgb = cpImage.reshape(cpImage.shape[0] * cpImage.shape[1], 3)
     # 使用pandas库统计list中不同(r,g,b)出现频率,可以通过索引查找元素
     imageRgbMap = pd.value_counts(imageRgb.tolist())
     # 将Series先转为list后再转为array便于后续直接一次访问多个下标数据
@@ -131,37 +164,121 @@ def generateRandRgbs(image, cnt):
     return imageRgbKeys[randIndex]
 
 
-# function: 根据shapes字段内容获取图像所有实例的mask的bool数组
+# function: 根据shapes字段内容获取图像所有实例的mask的bool数组及其分布边界位置
 # params: 图像尺寸，shapes字段内容
-# return: [mask0, mask1...] (type=array)
+# return: [mask0, mask1...] (type=array), minI, minJ, maxI, maxJ
 def getInstances(img_shape, shapes):
+    minI = []
+    minJ = []
+    maxI = []
+    maxJ = []
     instances = []
     for shape in shapes:
         points = shape["points"]
         shape_type = shape.get("shape_type", None)
         mask = shape_to_mask(img_shape[:2], points, shape_type)
-        # print(mask, mask.shape, type(mask))
         instances.append(mask)
-    return instances
+        # points中[x, y]是按照[长, 宽]，和image读取的(宽,长)不一样
+        pointsArr = np.array(points)
+        # 获取points中所有列(axis=0)的最值并添加到minI/minJ/maxI/maxJ中
+        minI.append(np.min(pointsArr, axis=0)[1])
+        minJ.append(np.min(pointsArr, axis=0)[0])
+        maxI.append(np.max(pointsArr, axis=0)[1])
+        maxJ.append(np.max(pointsArr, axis=0)[0])
+    return instances, int(np.min(np.array(minI))), int(np.min(np.array(minJ))),int(np.max(np.array(maxI))),int(np.max(np.array(maxJ)))
 
+
+# function: 根据所有染色体分布的边界minI,minJ,maxI,maxJ选取图像无染色体分布的四个角所有像素点
+# params: 图像，染色体分布上边界，左边界，下边界，右边界，返回的三维矩阵h，返回的三维矩阵w
+# return: [[[r1,g1,b1], [r2,g2,b2]...]]
+def cutImageNonChromos(image, minI, minJ, maxI, maxJ, cnt):
+    # LU，LD，RD，RU分别代表左上角，左下角，右下角，右上角四个角子图
+    fourImageSegs = []
+    fourImageSegs.append(image[handPadding[0]:minI, handPadding[1]:(image.shape[1]-handPadding[1])])
+    fourImageSegs.append(image[handPadding[0]:(image.shape[0]-handPadding[0]), handPadding[1]:minJ])
+    fourImageSegs.append(image[maxI:(image.shape[0]-handPadding[0]), handPadding[1]:(image.shape[1] - handPadding[1])])
+    fourImageSegs.append(image[handPadding[0]:image.shape[0]-handPadding[0], maxJ:(image.shape[1] - handPadding[1])])
+    fillPoints = fourImageSegs[0].reshape(-1)
+    fillPoints = np.concatenate((fillPoints, fourImageSegs[1].reshape(-1)), axis=0)
+    fillPoints = np.concatenate((fillPoints, fourImageSegs[2].reshape(-1)), axis=0)
+    fillPoints = np.concatenate((fillPoints, fourImageSegs[3].reshape(-1)), axis=0)
+    np.random.shuffle(fillPoints)
+    # fillPoints = np.concatenate((fillPoints, choosePoints), axis=0)
+    # np.random.shuffle(fillPoints)
+    return fillPoints[:cnt]
+
+
+# function: 根据非染色体区域对染色体区域进行覆盖填充
+# params: 图像，染色体分布上边界，左边界，下边界，右边界
+# return: 图像，待填充上边界，左边界，下边界，右边界
+def fillImageNonChromos(cpImage, minI, minJ, maxI, maxJ):
+    image = cpImage
+    # 选取填充区域的iU, iD, jL, jR
+    randLoc = [[handPadding[0],minI,handPadding[1], image.shape[1]-handPadding[1]], 
+                       [handPadding[0],image.shape[0]-handPadding[0], handPadding[1],minJ], 
+                       [maxI,image.shape[0]-handPadding[0], handPadding[1], image.shape[1]-handPadding[1]], 
+                       [handPadding[0],image.shape[0]-handPadding[0], maxJ, image.shape[1]-handPadding[1]]
+    ]
+    dire = rd.randint(0,3)
+    if dire == 0 or dire == 2:
+        offsetJ = rd.randint(handPadding[1]-minJ, image.shape[1]-handPadding[1]-maxJ)
+        if (maxI-minI) > (randLoc[dire][1]-randLoc[dire][0]): # 区域高度不够填充
+            cpImage[minI:(minI+randLoc[dire][1]-randLoc[dire][0]), minJ:maxJ] = image[randLoc[dire][0]:randLoc[dire][1], minJ+offsetJ:maxJ+offsetJ]
+            return cpImage, minI+randLoc[dire][1]-randLoc[dire][0], minJ, maxI, maxJ
+        else:
+            cpImage[minI:maxI, minJ:maxJ] = image[randLoc[dire][0]:(randLoc[dire][0]+maxI-minI), minJ+offsetJ:maxJ+offsetJ]
+            return cpImage, 0, 0, 0, 0
+    else:
+        offsetI = rd.randint(handPadding[0]-minI, image.shape[0]-handPadding[0]-maxI)
+        if maxJ-minJ > randLoc[dire][3]-randLoc[dire][2]: # 区域宽度不够填充
+            cpImage[minI:maxI, minJ:(minJ+randLoc[dire][3]-randLoc[dire][2])] = image[minI+offsetI:maxI+offsetI, randLoc[dire][2]:randLoc[dire][3]]
+            return cpImage, minI, minJ+randLoc[dire][3]-randLoc[dire][2], maxI, maxJ
+        else:
+            cpImage[minI:maxI, minJ:maxJ] = image[minI+offsetI:maxI+offsetI, randLoc[dire][2]:(randLoc[dire][2]+maxJ-minJ)]
+            return cpImage, 0, 0, 0, 0
+
+
+# function: 根据选区的背景图像，对其进行翻转生成更多的背景图像
+# params: 图像源路径，图像目标路径
+# return: null
+def rotateImages(srcPath, dstPath):
+    i = 0
+    imagePaths, _ = imgTool.ReadPath(srcPath)
+    for imagePath in imagePaths:
+        image = cv2.imread(imagePath)
+        # 水平翻转
+        horizonImage = image[:,::-1]
+        # 垂直翻转
+        verticalImage = image[::-1]
+        cv2.imwrite(dstPath + "/" + str(i) + ".png", image)
+        i += 1
+        cv2.imwrite(dstPath + "/" + str(i) + ".png", horizonImage)
+        i += 1
+        cv2.imwrite(dstPath + "/" + str(i) + ".png", verticalImage)
+        i += 1
 
 if __name__ == '__main__':
-    image = cv2.imread(
-        "/home/guest01/projects/chromos/utils/18-Y2087.145.O.JPG")
-    data = json.load(
-        open("/home/guest01/projects/chromos/utils/18-Y2087.145.O.json"))
-    lbl, ins = shapes_to_label(image.shape, data["shapes"],
-                               label_name_to_value)
-    # 获得的是一个二维bool数组，label=1的像素点值为true，1代表chromos标签
-    mask = lbl == 1
-
-    # instances = getInstances(image.shape, data["shapes"])
-    # for v in instances:
-    #     print(v, v.shape, image[v].shape[0])
-
+    srcPath = "/home/guest01/projects/chromos/utils/chromotest/cla1"
+    dstPath = "/home/guest01/projects/chromos/utils/chromotest_result/cla1"
+    rotateSrcPath = "/home/guest01/projects/chromos/utils/chromotest/labelme"
+    rotateDstPath = "/home/guest01/projects/chromos/utils/chromotest_result/augmentationSample"
+    jsonPaths, _ = imgTool.ReadPath(srcPath)
+    for jsonPath in jsonPaths:
+        data = json.load(open(jsonPath))
+        image = img_b64_to_arr(data.get("imageData"))
+        if len(image.shape)==3:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        instances, minI, minJ, maxI, maxJ = getInstances(image.shape, data["shapes"])
+        # 选取染色体区域的上下侧相同宽度填充部分染色体区域
+        cpImage = image.copy()
+        while minI<maxI and minJ<maxJ:
+            cpImage, minI, minJ, maxI, maxJ = fillImageNonChromos(cpImage, minI, minJ, maxI, maxJ)
+        filenamePre, _, _, _ = imgTool.parsePath(jsonPath)
+        savePath = dstPath + "/" + filenamePre + ".png"
+        cv2.imwrite(savePath, cpImage)
+    # rotateImages(rotateSrcPath, rotateDstPath)
+    
+    # lbl, ins = shapes_to_label(image.shape, data["shapes"],label_name_to_value)
     # randRgbs = generateRandRgbs(image, image[mask].shape[0])
-    # print(randRgbs, randRgbs.shape, type(randRgbs))
-
-    lblsave("/home/guest01/projects/chromos/utils/lbl.png", lbl)
-    lblsave("/home/guest01/projects/chromos/utils/ins.png", ins)
-    cv2.imwrite("/home/guest01/projects/chromos/utils/mask.png", image)
+    # lblsave("/home/guest01/projects/chromos/utils/lbl.png", lbl)
+    # lblsave("/home/guest01/projects/chromos/utils/ins.png", ins)
